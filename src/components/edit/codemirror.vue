79cdc791fc6133a4c51979f497a2dd9b88248c98
dyl <1980897959@qq.com>
2022-08-18 23:36:05 +0800

    feat: 引入codemirror

---
+++ src/components/edit/codemirror.vue
@@ -0,0 +1,34 @@
+<template>
+  <hr />
+  <div ref="textarea" style="width: 100%; height: 500px"></div>
+</template>
+
+<script lang="ts" setup>
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+  doc: Object,
+});
+const { doc } = props;
+
+let textarea = ref();
+
+let startState = EditorState.create({
+  doc: JSON.stringify(doc, null, "\t"),
+  extensions: [keymap.of(defaultKeymap)],
+});
+
+onMounted(() => {
+  let view = new EditorView({
+    state: startState,
+    parent: textarea.value,
+  });
+  console.log("textarea", textarea);
+});
+</script>
+
+<style></style>
--- src/components/optionEdit.vue
+++ src/components/optionEdit.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     {{ option }}
+    <Codemirror :doc="option"></Codemirror>
   </div>
 </template>
 <script setup lang="ts">
+import Codemirror from "./edit/codemirror.vue";
 interface Props {
   option: any;
 }
